0caf80fda79a548117f1ce6f4e3524c019e27e30
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-04-01 09:22:25 -0300

    Implement method to get controller and action from url path

--- lib/simplemvc.rb
+++ lib/simplemvc.rb
@@ -3,7 +3,17 @@ require "simplemvc/version"
 module Simplemvc
   class Application
     def call(env)
-      [ 200, { "Content-Type" => "text/html" }, ["Hello"] ]
+      # env["PATH_INFO"] = "/pages/about" => "PagesController.send(:about)"
+      controller_class, action = get_controller_and_action(env)
+      response = controller_class.new.send(action)
+
+      [ 200, { "Content-Type" => "text/html" }, [ response ] ]
+    end
+
+    def get_controller_and_action(env)
+      _, controller_name, action = env["PATH_INFO"].split("/")
+      controller_name = controller_name.capitalize + "Controller"
+      [ Object.const_get(controller_name), action ]
     end
   end
 end
