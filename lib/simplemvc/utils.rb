668ccbbdb8009650ea5e2cc5382aa9bf5d416403
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-04-01 10:20:34 -0300

    Fix some regex at controller render method

--- lib/simplemvc/utils.rb
+++ lib/simplemvc/utils.rb
@@ -1,14 +1,14 @@
 class String
   def to_snake_case
     self.gsub("::", "/").
-      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2'). # FOOBar => foo_bar
-      gsub(/([a-z\d])(A-Z)/, '\1_\2'). #FO86OBar => fo_86_o_bar
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
       tr("-", "_").
       downcase
   end
 
   def to_camel_case
-    return self if self !~ "_" && self =~ /[A-Z]+*./
+    return self if self !~ /_/ && self =~ /[A-Z]+*./
     split('_').map { |str| str.capitalize }.join # snake_case => SnakeCase
   end
 end
